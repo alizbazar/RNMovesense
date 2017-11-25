5875b268ba1fa9a9c0467520d0f4d07e03550984
Tuomo Jokimies <tuomo@dearlucy.co>
2017-11-26 00:50:45 +0200

    Remove animation

--- app/SlowText.js
+++ app/SlowText.js
@@ -10,11 +10,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     position: 'absolute',
     top: '62%',
-    width: '100%',
-    zIndex: 99
-  },
-  text: {
-    backgroundColor: 'transparent',
+    zIndex: 99,
     color: '#fff',
     fontFamily: 'AvenirNext-Medium',
     fontSize,
@@ -31,37 +27,37 @@ export default class SlowText extends React.Component {
     this.state = {
       text: null
     };
-    this.changeText = _.debounce(this.changeText.bind(this), fadeDuration, {
-      leading: true,
-      trailing: false
-    });
+    // this.changeText = _.debounce(this.changeText.bind(this), fadeDuration, {
+    //   leading: true,
+    //   trailing: true
+    // });
   }
 
   componentWillReceiveProps(nextProps) {
     const { text } = this.state;
-    if (nextProps.text !== text) {
-      this.changeText(nextProps.text);
-    }
+    // if (nextProps.text !== text) {
+    //   this.changeText(nextProps.text);
+    // }
   }
 
   changeText(text) {
-    const { text: originalText } = this.state;
-    if (originalText) {
-      this.refs.text.fadeOut(fadeDuration);
-    }
-    setTimeout(() => {
-      this.setState({ text }, () => {
-        this.refs.text.fadeIn(fadeDuration);
-      });
-    }, originalText ? fadeDuration : 0);
+    // const { text: originalText } = this.state;
+    // if (originalText) {
+    //   this.refs.text.fadeOut(fadeDuration);
+    // }
+    // setTimeout(() => {
+    //   this.setState({ text }, () => {
+    //     this.refs.text.fadeIn(fadeDuration);
+    //   });
+    // }, originalText ? fadeDuration : 0);
   }
 
   render() {
-    const { text } = this.state;
+    const { text } = this.props;
     return (
-      <Animatable.View ref="text" style={styles.view}>
-        <Text style={styles.text}>{text}</Text>
-      </Animatable.View>
+      {/*<Animatable.View ref="text" style={styles.view}>*/}
+        <Text style={styles.view}>{text}</Text>
+      // </Animatable.View>
     );
   }
 }
